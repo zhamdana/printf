5ccedd544386d0ebe1435e479372799ed480f622
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-22 19:43:07 +0000

    Fifth commit

--- int_print.c
+++ int_print.c
@@ -1,3 +1,6 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * print_unsigned_number - print unsigned number
@@ -6,111 +9,111 @@
  */
 int print_unsigned_number(unsigned int n)
 {
-	int c = 0;
-	unsigned int op = n;
+	int count = 0;
+	unsigned int nb = n;
 
-	if (op <= 9)
+	if (nb <= 9)
 	{
-		_putchar(op + '0');
+		_putchar(nb + '0');
 		return (1);
 	}
-	if (op > 9)
+	if (nb > 9)
 	{
-		c = print_unsigned_number(op / 10) + 1;
-		_putchar(op % 10 + '0');
-		return (c);
+		count = print_unsigned_number(nb / 10) + 1;
+		_putchar(nb % 10 + '0');
+		return (count);
 	}
 	return (0);
 }
 /**
  * print_number - display the number contained in an int
  * @n: int to print
- * Return: number of char
+ * Return: return 0
  */
 int print_number(int n)
 {
-	unsigned int op;
-	int c = 0;
+	unsigned int nb;
+	int count = 0;
 
-	op = n;
+	nb = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		c++;
-		op = -op;
+		count++;
+		nb = -nb;
 	}
-	if (op <= 9)
+	if (nb <= 9)
 	{
-		c += _putchar(op + '0');
-		return (c);
+		count += _putchar(nb + '0');
+		return (count);
 	}
-	if (op > 9)
+	if (nb > 9)
 	{
-		c += print_number(op / 10) + 1;
-		_putchar(op % 10 + '0');
-		return (c);
+		count += print_number(nb / 10) + 1;
+		_putchar(nb % 10 + '0');
+		return (count);
 	}
 	return (0);
 }
 /**
  * _nbr_len - length of a number
  * @prmNumber: number
- * Return: length of the number
+ * Return: return length of the number
  */
+
 int _nbr_len(int prmNumber)
 {
-	int loop = 0, numr;
+	int cLoop = 0, number;
 
 	if (prmNumber == 0)
 		return (1);
-
-	numr = prmNumber;
-
-	if (numr < 0)
+	number = prmNumber;
+	if (number < 0)
 	{
-		numr *= -1;
-		loop++;
+		number *= -1;
+		cLoop++;
 	}
-	while (numr)
+	while (number)
 	{
-		numr /= 10;
-		loop++;
+		number /= 10;
+		cLoop++;
 	}
-	return (loop);
+	return (cLoop);
 }
+
 /**
  * _itoa - Convert an int to a string
  * @prmNumber: int to convert
- * Return: converted string
+ * Return: return string
  */
 char *_itoa(int prmNumber)
 {
 	char *s;
-	int loop;
-	long numr;
+	int cLoop;
+	long number;
 
-	numr = prmNumber;
-	loop = _nbr_len(numr);
-	s = malloc(sizeof(char) * loop + 1);
+	number = prmNumber;
+	cLoop = _nbr_len(number);
+	s = malloc(sizeof(char) * cLoop + 1);
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-	s[loop] = '\0';
+	s[cLoop] = '\0';
 
-	if (numr == 0)
+	if (number == 0)
 	{
 		s = "0";
 	}
-	else if (numr < 0)
+	else if (number < 0)
 	{
 		s[0] = '-';
-		numr *= -1;
+		number *= -1;
 	}
-	while (numr)
+	while (number)
 	{
-		s[--loop] = numr % 10 + 48;
-		numr /= 10;
+		s[--cLoop] = number % 10 + 48;
+		number /= 10;
 	}
 	return (s);
 }
