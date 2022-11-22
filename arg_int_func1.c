a7e4471b2a62775454e7042463843ba308ced275
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-22 19:50:16 +0000

    Seventh commit

--- arg_int_func1.c
+++ arg_int_func1.c
@@ -1,78 +1,80 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
 /**
- * print_int - print number
+ * print_nbr - print number
  * @ap: arg list
  * Return: return printed character number
  */
-int print_int(va_list ap)
+int print_nbr(va_list ap)
 {
 	return (print_number(va_arg(ap, int)));
 }
 /**
  * print_binary - print number binary base
  * @ap: arg list
- * Return: return add
+ * Return: return sum
  */
 int print_binary(va_list ap)
 {
 	char *str;
-	int add = 0;
-	unsigned int argt = va_arg(ap, unsigned int);
+	int sum = 0;
+	unsigned int argument = va_arg(ap, unsigned int);
 
-	if (!argt)
+	if (!argument)
 	{
-		add += _puts("0", 0);
-		return (add);
+		sum += _puts("0", 0);
+		return (sum);
 	}
-	str = convert_base(argt, 2, 0);
+	str = convert_base(argument, 2, 0);
 	if (!str)
 		return (0);
-	add = _puts(str, 0);
+	sum = _puts(str, 0);
 	free(str);
-	return (add);
+	return (sum);
 }
 /**
- * print_octal - print octal base
+ * print_octal - print number octal base
  * @ap: arg list
- * Return: return add
+ * Return: return sum
  */
 int print_octal(va_list ap)
 {
 	char *str;
-	int add;
+	int sum;
 
 	str = convert_base(va_arg(ap, unsigned int), 8, 0);
-	add = _puts(str, 0);
+	sum = _puts(str, 0);
 	free(str);
-	return (add);
+	return (sum);
 }
 /**
  * print_hexa_lower - print hexa lower
  * @ap: arg list
- * Return: return add
+ * Return: return sum
  */
 int print_hexa_lower(va_list ap)
 {
 	char *str;
-	int add;
+	int sum;
 
 	str = convert_base(va_arg(ap, unsigned int), 16, 0);
-	add = _puts(str, 0);
+	sum = _puts(str, 0);
 	free(str);
-	return (add);
+	return (sum);
 }
 /**
  * print_hexa_upper - print hexa upper
  * @ap: arg list
- * Return: return add
+ * Return: return sum
  */
 int print_hexa_upper(va_list ap)
 {
 	char *str;
-	int add;
+	int sum;
 
 	str = convert_base(va_arg(ap, unsigned int), 16, 1);
-	add = _puts(str, 0);
+	sum = _puts(str, 0);
 	free(str);
-	return (add);
+	return (sum);
 }
