9e3dd3bfecf5436033dc9913bc3783672e2ab4a5
Zaharadeen Hamdana <zaharadeenhamdana@gmail.com>
2022-11-22 19:08:00 +0000

    Third commit

---
+++ main.h
@@ -0,0 +1,44 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+int _printf(const char *format, ...);
+int print_character(va_list ap);
+int print_string(va_list ap);
+int print_int(va_list ap);
+int print_binary(va_list ap);
+int print_octal(va_list ap);
+int print_hexa_l(va_list ap);
+int print_hexa_u(va_list ap);
+int print_unsigned(va_list ap);
+int print_unprintable(va_list ap);
+int print_reverse(va_list ap);
+int print_ptr(va_list ap);
+int print_rot13(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+int print_number(int n);
+int print_unsigned_number(unsigned int n);
+int _putchar(char c);
+int _puts(char *str, int ascii);
+int _nbr_len(int prmNumber);
+int _strlen_recursion(char *s);
+int convert_alpha_numeric(int nb, int upper);
+char *convert_rot13(char *str);
+char *convert_base(unsigned long nb, unsigned int base, int upper);
+char *_strdup(char *str);
+char *convert_base_pointer(unsigned long p);
+/**
+ * conv_printf - struct for function conversion
+ * @c: string
+ * @f: pointer to function
+ */
+typedef struct conv_printf
+{
+	char *c;
+	int (*f)(va_list);
+} conv_func;
+#endif
